db8fd1dada481d46acf47e27a22df6c96ac76138
HadeSatanist <ap66484@gmail.com>
2022-12-30 14:44:15 -0300

    Arrumando o Header e a NavBar

--- Projetointerdisciplinar/Avbooks/src/components/Header.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './Header.module.css'
+import AvBookslogo from "../assets/images/logo.png"
 const Header = () => {
   return (
-    <header className={styles.header}>
-        <h1>AV.Books</h1>
-    </header>
+     <header className={styles.header}>
+         <img src={AvBookslogo}/>
+     </header>
   )
 }
 
--- Projetointerdisciplinar/Avbooks/src/components/NavBar.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/NavBar.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
-
   return (
-    <header className={styles.navbar}>
-          <ul className={styles.items}>
-            <ul>Best Sellers</ul>
-            <ul>E-Books</ul>
-            <ul>Lançamentos</ul>
-            <ul>Romances</ul>
-            <ul>Infantins</ul>
-            <ul>Mangás</ul>
-            <ul>HQ's</ul>
+    <header className={styles.navBar}>
+          <ul className={styles.paiItems}>
+            <ul className={styles.item}>Best Sellers</ul>
+            <ul className={styles.item}>E-Books</ul>
+            <ul className={styles.item}>Lançamentos</ul>
+            <ul className={styles.item}>Romances</ul>
+            <ul className={styles.item}>Infantins</ul>
+            <ul className={styles.item}>Mangás</ul>
+            <ul className={styles.item}>HQ's</ul>
           </ul>
     </header>
   )
