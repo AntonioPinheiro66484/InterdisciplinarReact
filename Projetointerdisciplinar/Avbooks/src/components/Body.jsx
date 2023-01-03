1ecbb500a164020147c92d2de1f86c22504b1834
HadeSatanist <ap66484@gmail.com>
2023-01-03 17:21:20 -0300

    Implementação do componente body

---
+++ Projetointerdisciplinar/Avbooks/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Body.module.css'
+
+const Body = ()=>{
+    return(
+        <div className={styles.body}>
+            <h1>Algo</h1>
+        </div>
+    )
+
+}
+export default Body
