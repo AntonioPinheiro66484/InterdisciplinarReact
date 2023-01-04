86f83bc0f298709bf1b2d772cb474b601d67e624
HadeSatanist <ap66484@gmail.com>
2023-01-04 16:05:39 -0300

    criando a estrutura do body

--- Projetointerdisciplinar/Avbooks/src/components/Body.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Body.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import styles from './Body.module.css'
-import AvBookslogo from '../assets/images/logo.png'
 
 const Body = ()=>{
     return(
         <div className={styles.body}>
-            <img src={AvBookslogo}/>
-            <img src={AvBookslogo}/>
-            <img src={AvBookslogo}/>
-            <img src={AvBookslogo}/>
-            <img src={AvBookslogo}/>
-            <img src={AvBookslogo}/>
-            <div>
-                <img src={AvBookslogo}/>
-            </div>
+            
         </div>
     )
 
