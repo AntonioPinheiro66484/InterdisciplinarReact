aeb2ee1d4995ed706f365f3a6b2b64e5c0493b73
HadeSatanist <ap66484@gmail.com>
2023-01-04 16:07:05 -0300

    criando os componentes de produto e estilizando eles

---
+++ Projetointerdisciplinar/Avbooks/src/components/Produtos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Produtos.module.css'
+
+const Produtos =()=>{
+    return(<div className='produto'>
+                <div>
+                    <img src="" alt=""/>
+                </div>
+
+                <div>
+                    <h3></h3>
+                    <h3></h3>
+                </div>
+        </div>
+    )
+
+}
+
+export default Produtos
