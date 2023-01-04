f9933086639102c2a30276a2e4391b7d1694310e
HadeSatanist <ap66484@gmail.com>
2023-01-04 18:02:02 -0300

    ajustes no body para receber os produtos

--- Projetointerdisciplinar/Avbooks/src/components/Body.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Body.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Body.module.css'
+import Produtos from './Produtos'
 
 const Body = ()=>{
-    return(
-        <div className={styles.body}>
-            
-        </div>
+    return(<div className={styles.body}>
+            <div className={styles.titulo}>Livros</div>
+                <Produtos/>
+            </div>
     )
 
 }
