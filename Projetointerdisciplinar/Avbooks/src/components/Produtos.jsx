61e873c7ec29a60de6d6e2cc68d10e536dbeeb88
HadeSatanist <ap66484@gmail.com>
2023-01-04 18:03:26 -0300

    componente produto e estilo implementado

--- Projetointerdisciplinar/Avbooks/src/components/Produtos.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Produtos.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import Botao from './Botao';
 import styles from './Produtos.module.css'
+import Livro1 from '../assets/images/Livros/O caminho Jedi.png'
 
 const Produtos =()=>{
-    return(<div className='produto'>
-                <div>
-                    <img src="" alt=""/>
+    return(
+        <div className={styles.caixa}>
+            <div className={styles.produto}>
+                        <div className={styles.imagem}>
+                            <img src={Livro1} alt="Livro O caminho Jedi"/>
+                        </div>
+                        <div className={styles.descricao}>
+                            <h3>O caminho Jedi</h3>
+                            <h3>R$ 60,00</h3>
+                            <Botao/>
+                        </div>
                 </div>
-
-                <div>
-                    <h3></h3>
-                    <h3></h3>
-                </div>
-        </div>
+            </div>
     )
 
 }
