53665cefb61d43be24ef7681f8208c48df7d8228
HadeSatanist <ap66484@gmail.com>
2023-01-04 18:02:51 -0300

    ajuste no botão, formato, tamanho, cor e borda

---
+++ Projetointerdisciplinar/Avbooks/src/components/Botao.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Botao.module.css'
+
+const Botao = () =>{
+    return(
+            <button className={styles.botao}>Ler mais</button>
+    )    
+}
+
+export default Botao
