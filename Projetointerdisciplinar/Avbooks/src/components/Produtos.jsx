d2b518a1b0e3c2f483356e42fbb88d9e8c7539f5
HadeSatanist <ap66484@gmail.com>
2023-01-05 17:53:41 -0300

    Ajustes no produto

--- Projetointerdisciplinar/Avbooks/src/components/Produtos.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Produtos.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import Botao from './Botao';
 import styles from './Produtos.module.css'
-import Livro1 from '../assets/images/Livros/O caminho Jedi.png'
+import Livro1 from '../assets/images/Livros/Gravity Falls - O diário perdido.png'
+import Livro2 from '../assets/images/Livros/A Arte da Guerra.png'
+import Livro3 from '../assets/images/Livros/It- A coisa.png'
+
+
+
 
 const Produtos =()=>{
     return(
-        <div className={styles.caixa}>
-            <div className={styles.produto}>
-                        <div className={styles.imagem}>
-                            <img src={Livro1} alt="Livro O caminho Jedi"/>
+            <div className={styles.caixa}>
+                    <div className={styles.produto}>
+                                <div className={styles.imagem}>
+                                    <img src={Livro1} alt="Gravity Falls - O diário perdido"/>
+                                </div>
+                                <div className={styles.descricao}>
+                                    <h3>Gravity Falls - <br/>O diário perdido</h3>
+                                    <h3>R$ 60,00</h3>
+                                    <Botao/>
+                                </div>
                         </div>
-                        <div className={styles.descricao}>
-                            <h3>O caminho Jedi</h3>
-                            <h3>R$ 60,00</h3>
-                            <Botao/>
+
+                        <div className={styles.produto}>
+                                <div className={styles.imagem2}>
+                                    <img src={Livro2} alt="A Arte da Guerra"/>
+                                </div>
+                                <div className={styles.descricao}>
+                                    <h3>A Arte da Guerra</h3>
+                                    <h3>R$ 60,00</h3>
+                                    <Botao/>
+                                </div>
                         </div>
-                </div>
-            </div>
+                        
+                        <div className={styles.produto}>
+                                <div className={styles.imagem3}>
+                                    <img src={Livro3} alt="It- A coisa"/>
+                                </div>
+                                <div className={styles.descricao}>
+                                    <h3>It - A coisa</h3>
+                                    <h3>R$ 60,00</h3>
+                                    <Botao/>
+                                </div>
+                        </div>
+                    </div>
+            
     )
 
 }
