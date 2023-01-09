6f26c13fdd13e98b4cebab77b0f1a6db51009ca7
HadeSatanist <ap66484@gmail.com>
2023-01-09 16:28:03 -0300

    agrupamento de produtos. falta arrumar ainda o posicionamento do preço relativo ao nome

--- Projetointerdisciplinar/Avbooks/src/components/Produtos.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Produtos.jsx
@@ -5,9 +5,6 @@ import Livro1 from '../assets/images/Livros/Gravity Falls - O diário perdido.pn
 import Livro2 from '../assets/images/Livros/A Arte da Guerra.png'
 import Livro3 from '../assets/images/Livros/It- A coisa.png'
 
-
-
-
 const Produtos =()=>{
     return(
             <div className={styles.caixa}>
@@ -16,30 +13,30 @@ const Produtos =()=>{
                                     <img src={Livro1} alt="Gravity Falls - O diário perdido"/>
                                 </div>
                                 <div className={styles.descricao}>
-                                    <h3>Gravity Falls - <br/>O diário perdido</h3>
-                                    <h3>R$ 60,00</h3>
+                                    <h3 className={styles.nomeLivro}>Gravity Falls <br/></h3>
+                                    <h3 className={styles.precoLivro}>R$ 60,00</h3>
                                     <Botao/>
                                 </div>
                         </div>
 
                         <div className={styles.produto}>
-                                <div className={styles.imagem2}>
+                                <div className={styles.imagem}>
                                     <img src={Livro2} alt="A Arte da Guerra"/>
                                 </div>
                                 <div className={styles.descricao}>
-                                    <h3>A Arte da Guerra</h3>
-                                    <h3>R$ 60,00</h3>
+                                    <h3 className={styles.nomeLivro}>A Arte da Guerra</h3>
+                                    <h3 className={styles.precoLivro}>R$ 60,00</h3>
                                     <Botao/>
                                 </div>
                         </div>
                         
                         <div className={styles.produto}>
-                                <div className={styles.imagem3}>
+                                <div className={styles.imagem}>
                                     <img src={Livro3} alt="It- A coisa"/>
                                 </div>
                                 <div className={styles.descricao}>
-                                    <h3>It - A coisa</h3>
-                                    <h3>R$ 60,00</h3>
+                                    <h3 className={styles.nomeLivro}>It - A coisa</h3>
+                                    <h3 className={styles.precoLivro}>R$ 60,00</h3>
                                     <Botao/>
                                 </div>
                         </div>
