468aed214946966641e46e06ecde0343c898fff5
HadeSatanist <ap66484@gmail.com>
2023-01-10 18:01:38 -0300

    quase finalização do header

--- Projetointerdisciplinar/Avbooks/src/components/Header.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './Header.module.css'
 import AvBookslogo from "../assets/images/Imagens/logo.png"
+import Perfil from '../assets/icons/perfil.png'  
+import Carrinho from '../assets/icons/carrinho.png'
+import Lupa from '../assets/icons/lupa.png'
 const Header = () => {
   return (
      <header className={styles.header}>
          <img className={styles.logo} src={AvBookslogo}/>
+         <input img src={Lupa} className={styles.barraPesquisa} type="text"/>
+         <div className={styles.icons}>
+            <img className={styles.icon} src={Perfil} alt="Icone de perfil do cliente"/>
+            <img className={styles.icon} src={Carrinho} alt="icone de carrinho do cliente"/>
+         </div>
       </header>
   )
 }
