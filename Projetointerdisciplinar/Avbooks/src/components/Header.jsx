9729430e620e41470ff37f415eaff88ad8f82844
HadeSatanist <ap66484@gmail.com>
2023-01-12 17:58:21 -0300

    reestruturação do codigo para colocar a barra de pesquisa com o a caixa e a lupa uniformes

--- Projetointerdisciplinar/Avbooks/src/components/Header.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Header.jsx
@@ -8,7 +8,12 @@ const Header = () => {
   return (
      <header className={styles.header}>
          <img className={styles.logo} src={AvBookslogo}/>
-         <input img src={Lupa} className={styles.barraPesquisa} type="text"/>
+         <div className={styles.quadrado}>
+            <input className={styles.barraPesquisa} type="text"/>
+            <div className={styles.caixaLupa}>
+               <img className={styles.lupa} src={Lupa} alt="Icone de lupa"/>
+            </div>
+         </div>
          <div className={styles.icons}>
             <img className={styles.icon} src={Perfil} alt="Icone de perfil do cliente"/>
             <img className={styles.icon} src={Carrinho} alt="icone de carrinho do cliente"/>
