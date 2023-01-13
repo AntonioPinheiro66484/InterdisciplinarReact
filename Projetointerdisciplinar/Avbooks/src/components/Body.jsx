2446f17bac6f71b80cf23960ce1f7f0e93ce48aa
HadeSatanist <ap66484@gmail.com>
2023-01-13 16:58:41 -0300

    ajuste no body espaçando os produtos

--- Projetointerdisciplinar/Avbooks/src/components/Body.jsx
+++ Projetointerdisciplinar/Avbooks/src/components/Body.jsx
@@ -4,15 +4,15 @@ import Produtos from './Produtos'
 
 const Body = ()=>{
     return(<div className={styles.body}>
-        <div className={styles.mostruario}>
+        <div className={styles.categorias}>
             <div className={styles.titulo}>Novidades</div>
                 <Produtos/>
         </div >
-        <div className={styles.mostruario}>
+        <div className={styles.categorias}>
             <div className={styles.titulo}>Populares</div>
                 <Produtos/>
         </div>
-        <div className={styles.mostruario}>
+        <div className={styles.categorias}>
             <div className={styles.titulo}>Clássicos</div>
                 <Produtos/>
         </div>
